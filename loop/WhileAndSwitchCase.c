4361861080d30fa08897db6d80d594151e12d51f
rkkarmur <karmurramesh8589@gmail.com>
2022-01-08 19:08:18 +0530

    [continue op] cantinue calculator

---
+++ loop/WhileAndSwitchCase.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int  a, b;
+    char op;
+    while (op != '@')
+    {
+        printf("\nEnter operation: ");
+        scanf("%s", &op);
+        printf("Enter 1st no: ");
+        scanf("%d", &a);
+        printf("\nEnter 2st no: ");
+        scanf("%d", &b);
+
+        switch (op)
+        {
+        case '+':
+
+            printf("%d %c %d = %d", a, op, b, a + b);
+            break;
+
+        case '-':
+
+            printf("%d %c %d = %d", a, op, b, a - b);
+            break;
+
+        case '*':
+
+            printf("%d %c %d = %d", a, op, b, a * b);
+            break;
+
+        case '/':
+
+            printf("%d %c %d = %d", a, op, b, a / b);
+            break;
+
+        default:
+            printf("op is not velid");
+            break;
+        }
+    }
+    return (0);
+}
